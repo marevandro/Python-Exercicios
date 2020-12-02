343b6367c63493d92a6a612d8e8f3472c07b05e5
Marevandro <62778708+marevandro@users.noreply.github.com>
2020-12-02 08:01:51 -0300

    Update aula#6.py
    Exercícios durante a aula 6 para compreensão do tipos primitivos.

--- aula#6.py
+++ aula#6.py
@@ -1,1 +1,17 @@
+n1 = int(input("Digite um numero: "))
+n2 = int(input("Digite outro numero: "))
+result = n1 + n2
+print (result)
+'''Para tipos inteiros usamos int, lembrando que a classificação deve ser feita antes do input.
+Numeros positivos, negativos ou nulos, são inteiros se não tiverem .'''
+
+'''obs: Em Python os numeros não usamos a vírgula indicando que o algarismo a seguir 
+pertence à ordem das décimas, ou casas decimais, é ultilizado o ponto (.) para fazer essa divisão. '''
+
+n1 = float(input("Digite um numero: "))
+n2 = float(input("Digite outro numero: "))
+result = n1 + n2
+print (result)
+'''Para numeros com ponto flutuante ou numeros reais, que precisam de um ponto para separar das casas decimais.'''
+
 
