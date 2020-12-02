8298eccf6e92730b36131372a41ce48936d74aec
Marevandro <62778708+marevandro@users.noreply.github.com>
2020-12-02 07:38:17 -0300

    Execicio 2
    Alguns comentários e uma variação do print usando uma variável.

--- 100 Exercicios/EX-02.py
+++ 100 Exercicios/EX-02.py
@@ -1,4 +1,7 @@
-num1 = input("Digite um numero: ")
-num2 = input("Digite outro numero: ")
-result = num1 + num2
-print = (result)
+nome = input("Digite seu nome: ") #Lembrando que input sempre deve seguir quando precisamos da entrada do usuário.
+print ("Bom dia", nome,"!") # Seguindo a logica, deixei o nome fora das aspas duplas e deu certo.
+print ("É um prazer te conhecer {}".format(nome)) 
+'''Primeiramente, o método format() em python serve basicamente para criar uma string que contém campos entre 
+chaves que são substituidos pelos argumentos de format. ... Vale ressaltar que a numeração dos parâmetros
+na string inicia pelo numero zero para o primeiro parâmetro, 1 para o segundo parâmetro e assim por diante.'''
+
