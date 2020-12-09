092bc7c48a1a94f021b1f1cf8d43b50c6b7c562b
Marevandro <marevandro95@gmail.com>
2020-12-08 21:57:32 -0300

    metodo format
    metodo format

---
+++ aulas/aula#6.b.py
@@ -0,0 +1,16 @@
+n1 = float(input("Digite um numero: "))
+n2 = float(input("Digite outro numero: "))
+result = n1 + n2
+print (result)
+print('A soma de pontos flutuantes entre', n1, 'e', n2,'é igual a,', result)
+'''Para numeros com ponto flutuante ou numeros reais, que precisam de um ponto para separar das casas decimais.'''
+
+n1 = bool(input("Digite um numero: "))
+print(n1)
+'''O valor boleano nessa interpretação apenas acusa se e digitado algo no input.'''
+
+#A outras formas de trabalhar o tipo booleano
+
+n3 = input('Digite algo: ')
+print(n3.isnumeric())
+#informa se o que foi digitado e um número.
--- aulas/aula#6.py
+++ aulas/aula#6.py
@@ -2,22 +2,13 @@ n1 = int(input("Digite um numero: "))
 n2 = int(input("Digite outro numero: "))
 result = n1 + n2
 print (type(result))
+print('A soma entre {} e {} vale {}'.format(n1, n2, result))
 '''Para tipos inteiros usamos int, lembrando que a classificação deve ser feita antes do input.
 Numeros positivos, negativos ou nulos, são inteiros se não tiverem .'''
 
 '''obs: Em Python os numeros não usamos a vírgula indicando que o algarismo a seguir 
 pertence à ordem das décimas, ou casas decimais, é ultilizado o ponto (.) para fazer essa divisão. '''
 
-n1 = float(input("Digite um numero: "))
-n2 = float(input("Digite outro numero: "))
-result = n1 + n2
-print (result)
-'''Para numeros com ponto flutuante ou numeros reais, que precisam de um ponto para separar das casas decimais.'''
-
-n1 = bool(input("Digite um numero: "))
-n2 = bool(input("Digite outro numero: "))
-result = n1 + n2 == True
-print (result)
 
 n1 = str(input("Digite um numero: "))
 n2 = str(input("Digite outro numero: "))
