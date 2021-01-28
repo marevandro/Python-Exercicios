b6d85bacdd18491d67280847985ad9168552d8fd
Marevandro <marevandro95@gmail.com>
2021-01-27 21:04:18 -0300

    EX-16
    Resolução do Exercicio 16.

--- 100 Exercicios/EX-16.py
+++ 100 Exercicios/EX-16.py
@@ -3,7 +3,16 @@ na tela a sua porção INTEIRA.
 EX: Digite um número: 6.127
 O número 6.127 tem a parte inteira 6.'''
 
-from math import floor, ceil
+'''
+from math import floor, ceil, trunc
 num = float(input("Digite um número: "))
 print('O resultado inteiro do numero {} é {}'.format(num, floor(num)))
 print('Arredondando pra cima, fica: {}.'.format(ceil(num)))
+
+num1 = float(input('Digite um número: '))
+print ('O resultado inteiro do número {} é {}.'.format(num1, trunc(num1)))
+'''
+#sem usar uma biblioteca para esse exercicio.
+num = float(input("Digite um valor: "))
+print('O valor digitanto foi {} e sua porção inteira é {}.'.format(num, int(num)))
+
