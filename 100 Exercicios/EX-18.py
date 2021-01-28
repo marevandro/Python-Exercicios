a9ab1d31523027cb5ceff29271d786c938b5b874
Marevandro <marevandro95@gmail.com>
2021-01-27 22:48:02 -0300

    conclusão do ex 17 e 18.
    conclusão do ex 17 e 18.
    Correção no ex  18, precisa ser passado pra radiando depois de calcular o seno, cosseno e tangente.

--- 100 Exercicios/EX-18.py
+++ 100 Exercicios/EX-18.py
@@ -1,10 +1,23 @@
 '''Faça um programa que leia um ângulo qualquer e mostre
 na tela o valor do seno, cosseno e tangente dessse ângulo'''
-from math import sin, cos, tan
-num = float(input('Digite o ângulo que você deseja: '))
-seno = sin(num)
-cosseno = cos(num)
-tangente = tan(num)
-print ('O seno do angulo {} é {:.2}'.format(num, seno))
-print('O cosseno do ângulo {} é {:.2}'.format(num, cosseno))
-print('A tangente do ângulo {} é {:.2}'.format(num, tangente))
+
+'''import math
+angulo = float(input('Digite o ângulo que você deseja: '))
+seno = math.sin(math.radians((angulo)))
+print('{}° tem o SENO de {:.2f}'.format(angulo, seno))
+consseno = math.cos(math.radians(angulo))
+print('{}° tem o cosseno de {:.2f}'.format(angulo, consseno))
+tangente = math.tan(math.radians(angulo))
+print('{}º tem a tangente de {:.2f}'.format(angulo, tangente))'''
+
+#importando toda a biblioteca math
+
+
+from math import sin, cos, tan, radians
+ang = float(input('Digite o ângulo que você deseja: '))
+seno = sin(radians(ang))
+cosseno = cos(radians(ang))
+tangente = tan(radians(ang))
+print ('O seno do angulo {} é {:.2}'.format(ang, seno))
+print('O cosseno do ângulo {} é {:.2}'.format(ang, cosseno))
+print('A tangente do ângulo {} é {:.2}'.format(ang, tangente))
