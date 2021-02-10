f9017566695b2bdcf0b95aece7b0d22985ef5a38
Marevandro <marevandro95@gmail.com>
2021-02-10 15:27:28 -0300

    Fazer validação do erro.
    Realizar validação do erro e solução, dando informação mais clara sobre o porque da música não tocar e o programa rodar sem erros.

--- 100 Exercicios/EX-21.py
+++ 100 Exercicios/EX-21.py
@@ -1,7 +1,15 @@
 '''Faça um programa em Python que abra
  e reproduza o áudio de um arquivo MP3.'''
-import pygame
+'''import pygame
 pygame.init()
 pygame.mixer.music.load('EX-21remix.mp3')
 pygame.mixer.music.play()
-pygame.event.wait()
+pygame.event.wait()'''
+
+'''Explicação do primerio erro: https://pt.stackoverflow.com/questions/288050/m%c3%basica-n%c3%a3o-toca'''
+
+import pygame
+pygame.mixer.init()
+pygame.mixer.music.load('EX-21seujorge.mp3')
+pygame.mixer.music.play()
+x = input('Digite algo quando quiser parar a música: ')
