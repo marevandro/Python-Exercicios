d5047fbf3234072e05f519e73ce9a0b23b11a8c7
Marevandro <marevandro95@gmail.com>
2021-02-17 23:12:04 -0300

    Resolução do EX22
    Não obtive entendimento no método "find" para contar quantas letras tem o primeiro nome.

--- 100 Exercicios/EX-22.py
+++ 100 Exercicios/EX-22.py
@@ -2,6 +2,24 @@
 
 – O nome com todas as letras maiúsculas e minúsculas.
 
-– Quantas letras ao tod (sem considerar espaços).
+– Quantas letras ao tdo (sem considerar espaços).
 
 – Quantas letras tem o primeiro nome.'''
+
+nome = str(input('Digite seu nome:')).strip()
+print(f'Ola {nome}.')
+print(f'Seu nome em maiúsculo fica {nome.upper()}!')
+print(f'Seu nome em minúsculo fica {nome.lower()}!')
+
+#aqui contamos quantos espaços tem e diminuimos
+print(f"Seu nome tem no total de {len(nome) - nome.count(' ')} letras")
+
+print('-'*23)
+print("""No curso foi abordado como resolver essa segunda parte usando o metodo "find",
+mas não ficou claro, quem poder ajudar no entendimento eu agradceço.
+Usei a segunda abordagem, separando e contando com o len.""")
+print('Sintaxe: print(f"Seu nome tem {nome.find(' ')} letras")')
+print('-'*23)
+
+primeiro = nome.split()
+print(f'Seu primeiro nome tem {len(primeiro[0])} letras')
