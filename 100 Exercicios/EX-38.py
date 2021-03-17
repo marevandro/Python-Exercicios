8c9f54477b93bd9bcea6ddae093bf993afb8fe43
Marevandro <marevandro95@gmail.com>
2021-03-17 18:52:19 -0300

    Update EX-38.py
    resolução do curso

--- 100 Exercicios/EX-38.py
+++ 100 Exercicios/EX-38.py
@@ -5,6 +5,7 @@
 – O segundo valor é maior
 
 – Não existe valor maior, os dois são iguais'''
+#minha resolução
 from time import sleep
 num1 = int(input('Digite o primeiro número: '))
 num2 = int(input('Digite o segundo número: '))
@@ -16,3 +17,13 @@ elif num1 < num2:
     print(f'O segundo número digitado {num2}, é maior que {num1}')
 else:
     print("Os dois números tem o mesmo valor!")
+
+#resolução do curso
+n1 = int(input('Primeiro número: '))
+n2 = int(input('Segundo número: '))
+if n1 > n2:
+    print('O PRIMEIRO valor é maior')
+elif n2 > n1:
+    print('O SEGUNDO valor é maior')
+else:
+    print('Os dois valores são IGUAIS')
