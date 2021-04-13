57e5b550f6b6a44fe3849606735243b7456eb44e
Marevandro <62778708+marevandro@users.noreply.github.com>
2021-04-13 18:44:27 -0300

    Update EX-50.py

--- 100 Exercicios/Mundo 2/EX-50.py
+++ 100 Exercicios/Mundo 2/EX-50.py
@@ -1,1 +1,13 @@
+'''Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares.
+Se o valor digitado for ímpar, desconsidere-o.'''
 
+#minha resolução
+numpar = 0
+
+for par in range(1,7):
+    num = int(input('Digite um número: '))
+    if num % 2==0:
+        numpar = num + numpar
+        listapar= numpar
+    else:
+   '''A construção da repetição esta ok, ele ja soma os pares.'''
