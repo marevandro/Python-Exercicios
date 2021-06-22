589727bc89270d8e8a7862b769dc9adeb9c5ccb0
Marevandro <62778708+marevandro@users.noreply.github.com>
2021-06-22 11:37:59 -0300

    Update EX-61 - Progressão Aritmética v2.0.py

--- 100 Exercicios/Mundo 2/EX-61 - Progressão Aritmética v2.0.py
+++ 100 Exercicios/Mundo 2/EX-61 - Progressão Aritmética v2.0.py
@@ -1,2 +1,21 @@
 '''Refaça o DESAFIO 51, lendo o primeiro termo e a razão de 
 uma PA, mostrando os 10 primeiros termos da progressão usando a estrutura while.'''
+
+#primeiro = int(input('Primeiro termo: '))
+#razão = int(input('Razão: '))
+#for c in range(1, 10, 2):
+#    print(f'{c}')
+#print('Acabou.')
+
+
+#resolução do curso
+print ('Gerador de PA')
+print('-='*10)
+primeiro = int(input('Primeiro termo: '))
+razão = int(input('Razão da PA: '))
+termo = primeiro
+cont = 1
+while cont <= 10:
+    print(f'{termo}')
+    termo += razão
+    cont += 1
