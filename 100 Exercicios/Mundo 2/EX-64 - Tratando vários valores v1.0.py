c016fc1dc63736b6d16a6835c095f55acb6825cd
Marevandro <62778708+marevandro@users.noreply.github.com>
2021-06-30 18:15:00 -0300

    Create EX-64 - Tratando vários valores v1.0.py

---
+++ 100 Exercicios/Mundo 2/EX-64 - Tratando vários valores v1.0.py
@@ -0,0 +1,24 @@
+'''Crie um programa que leia vários números inteiros pelo teclado. 
+O programa só vai parar quando o usuário digitar o valor 999, que é a condição de parada. 
+No final, mostre quantos números foram digitados e qual foi a soma entre eles (desconsiderando o flag).'''
+
+
+#minha resolução
+print('-'*5,'Leitor de números','-'*5)
+cont = 0
+num = 0
+soma = 0
+while num != 999:
+        num = int(input('Digite um número: '))
+        print('Digitando 999 o programa vai parar')
+        cont += 1
+        soma = num + soma
+        if num == 999:
+            soma -= 999
+            cont -=1
+print(f'Você digitou {cont} números, a soma desse números é {soma}')  
+print('FIM')
+
+
+#resolução do curso
+
