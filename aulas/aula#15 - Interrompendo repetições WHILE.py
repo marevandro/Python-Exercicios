da68da86de36f86bfbd9ed2b240e590f13b4202e
Marevandro <marevandro95@gmail.com>
2021-07-31 12:07:38 -0300

    Create aula#15 - Interrompendo repetições WHILE.py

---
+++ aulas/aula#15 - Interrompendo repetições WHILE.py
@@ -0,0 +1,15 @@
+'''n = s =  0
+while  n != 999:
+    n = int(input('Digite um número: '))
+    s += n
+s -= 999
+print ('A soma vale {}'.format(s))'''
+
+
+n = s = 0
+while True:
+    n = int(input('Digite um número: '))
+    if n == 999:
+        break
+    s += n
+print ('A soma vale {}'.format(s))
