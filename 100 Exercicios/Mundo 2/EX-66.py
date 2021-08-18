451075d80d3b9c1a6771af71c20c01e32e67fd5d
Marevandro <62778708+marevandro@users.noreply.github.com>
2021-08-18 10:39:10 -0300

    Update EX-66.py

--- 100 Exercicios/Mundo 2/EX-66.py
+++ 100 Exercicios/Mundo 2/EX-66.py
@@ -5,10 +5,10 @@ que é a condição de parada. No final, mostre quantos números
 foram digitados e qual foi a soma entre elas (desconsiderando o flag).
 '''
 
-
+#minha resolução
 print('=*='*10)
 conta = soma = 0
-
+Print('Vamos somar os números e quando quiser parar digite 999 ')
 while True:
     num = int(input('Digite um número: '))
     if num == 999:
