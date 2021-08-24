96fc059dc8aeaf65c6e2aabad93f3b0607e5deaa
Marevandro <62778708+marevandro@users.noreply.github.com>
2021-08-23 22:03:46 -0300

    Correção
    Feito correção porém preciso refazer as variáveis da minha solução.

--- 100 Exercicios/Mundo 2/EX-70 - Estatísticas em produtos.py
+++ 100 Exercicios/Mundo 2/EX-70 - Estatísticas em produtos.py
@@ -7,34 +7,68 @@ B) quantos produtos custam mais de R$1000.
 C) qual é o nome do produto mais barato.'''
 
 #minha solução
-total_com = mais_mil = menor_produto = 0
-maior = menor = 0
+total_com = mais_mil = menor_produto = maior = 0
+result_produto = ' '
 print('-=-'*7,'MERCADINHO DEV','-=-'*7)
 while True:
     nome_produ = str(input('Digite o nome do produto: '))
-    valor = float(input('Agora digite o valor: R$ '))
+    valor = float(input('Agora digite o valor: R$'))
     result = ' '
     while result not in 'SN':
-        result = input('Deseja continuar comprando [S/N] ?' ).strip().upper()
+        result = input('Deseja continuar comprando [S/N] ?' ).strip().upper()[0]
     total_com += valor
-    if valor < 1000:
+    if valor > 1000:
         mais_mil += 1
-	
-	
-	if total_com == 1:
-		  reult_produ = nome_produ
-	    maior  = valor
-  else:
-      if maior < valor:
-      result_produ = nome_produ
-			
-	
+    
+    
+    if total_com == 1 or valor < menor_produto:
+        menor_produto = valor
+        maior  = valor
+        result_produ = nome_produto
+            
+    
     print('---\n'*3)
-	
-	
+    
+    
     if result == 'N':
         break
-print(f'Total de produtos {total_com}')
+print(f'Total de produtos {total_com:.2f}')
 print(f'Total de produtos acima de R$ 1000,00 é {mais_mil}')
 print(f'O produto mais barato é {reult_produ}')
+
+
+#resolução do curso
+total = totmil = menor = cont = 0
+barato = ' '
+while True:
+	produto = str(input('Nome do Produto: '))
+	preço = float(input('Preço: R$'))
+	cont += 1
+	total += preço
+	if preço > 1000:
+		totmil += 1
+	
+	#esse bloco se repete 
+	#if cont == 1:
+	#	menor = preço
+	#	barato = produto
+	#else:
+	#	if preço < menor:
+	#		menor = preço
+	#		barato = produto
+	
+	#segunda opção
+	if cont == 1 or preço < menor:
+		menor = preço
+		barato = produto 
+	
+	resp = ' '
+	while resp not in 'SN':
+		resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+	if resp == 'N':
+		break
+print('{:-^40}'.format('FIM DO PROGRAMA'))
+print(f'O total da compra foi R${total:.2f}')
+print(f'Temos {} custando mais de R$ 1000,00')
+print(f'O produto mais barato foi {barato} que custa R${menor:.2f}')
 
