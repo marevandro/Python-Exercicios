afb95d8d90dae929efdfa9cb37533d63157abe85
Marevandro <62778708+marevandro@users.noreply.github.com>
2021-08-27 10:44:23 -0300

    Update aula#16 - Tuplas.py
    Exemplos da aula de TUPLAS

--- aulas/aula#16 - Tuplas.py
+++ aulas/aula#16 - Tuplas.py
@@ -1,1 +1,33 @@
+lanche = ('Hambúrguer','Suco','Pizza','Pudim','Batata Frita')
 
+#EXEMPLO 1
+for comida in lanche:
+	print(f'Eu vou comer {comida}')
+
+#EXEMPLO 2 / SEMPRE PRECISAR MOSTRAR A POSIÇÃO
+for cont in range(0, len(lanche)):
+	print(f'Eu vou comer {lanche[cont]} na posição {cont}')
+
+#EXEMPLO 3 
+for pos, comida in enumerate(lanche):
+	print(f'Eu vou comer {comida} na posição {pos}')
+
+
+
+#EXEMPLO 4 / SORTED PARA CRIAR UMA ORDEM ALFABETICA
+print(sorted(lanche))
+
+print('Comi pra caramba!')
+
+
+#A TUPLA NÃO SOMA VALORES, CONCATENA 
+a = (2, 5, 4)
+b = (5, 8, 1, 2)
+c = a + b	
+print(c)
+print(c.count(4))
+print(c.index(8)) #MOSTRA A POSIÇÃO
+
+
+#POSSO TER DADOS DE TIPOS DIFERENTES DENTRO DA TUPLA
+#A TUPLA E IMUTÁVEL, MAS CONSEGUIMOS APAGAR USANDO O 'del', MAS NÃO CONSIGO DELETAR UM ITEM
