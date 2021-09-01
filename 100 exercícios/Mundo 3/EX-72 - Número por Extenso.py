9abce292149a2f9708232564e1ed7da3a965d861
Marevandro <marevandro95@gmail.com>
2021-08-31 22:46:04 -0300

    Edição
    Edição

--- 100 exercícios/Mundo 3/EX-72 - Número por Extenso.py
+++
@@ -1,7 +0,0 @@
-''' Crie um programa que tenha uma dupla totalmente preenchida com uma contagem por 
-extenso, de zero até vinte. Seu programa deverá ler um número pelo teclado 
-(entre 0 e 20) e mostrá-lo por extenso.'''
-num_ext = ('Zero','Um','Dois','Três','Quatro','Cinco','Seis','Sete','Oito','Nove','Dez','Onze','Doze','Treze','Quatorze','Quinze','Dezesseis','Dezessete','Dezoito','Dezenove','Vinte')
-#while ler_num not in ':
-	#ler_num = ' '
-ler_num = int(input('Digite um número: '))
