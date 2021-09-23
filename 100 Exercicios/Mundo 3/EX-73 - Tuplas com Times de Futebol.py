bc4e10f28612ff5c8b75df956218d485cac7f27d
Marevandro <62778708+marevandro@users.noreply.github.com>
2021-09-22 22:04:51 -0300

    Update EX-73 - Tuplas com Times de Futebol.py

--- 100 Exercicios/Mundo 3/EX-73 - Tuplas com Times de Futebol.py
+++ 100 Exercicios/Mundo 3/EX-73 - Tuplas com Times de Futebol.py
@@ -9,7 +9,6 @@ c) Times em ordem alfabética.
 
 d) Em que posição está o time da Chapecoense.'''
 
-#minha resolução
 grupoA = ('Aslborg','Kiel','Montpellier','Szeged','Elverum','Vardar 1961',
 		  'Meshkov Brest','PPD Zagred')
 
@@ -29,35 +28,54 @@ while grupo not in 'AB':
     grupo = str(input('Grupo A ou B: ')).strip().upper()
     
     if grupo == 'A':
-        print('GRUPO A SELECIONADO: ')
+        print('Grupo A selecionado: ')
 		#A) OS 5 PRIMEIROS TIMES.
 		print()
-		print(f'Os 5 primeiros times são {grupoA[0:5]}')
+		print(f'Os 5 primeiros times do grupo A são: {grupoA[0:5]}')
 		#B) OS ÚLTIMOS 4 COLOCADOS.
-		print(f'Os últimos 4 times na zona de rebaixamento são {grupoA[-4:]}')
+		print(f'Os últimos 4 times do grupo A são: {grupoA[-4:]}')
 		print()
 		#C) TIMES EM ORDEM ALFABÉTICA.
 		print(f'Sequência de times em ordem alfabética: {sorted(grupoA)}')
 		print()
 		#D) EM QUE POSIÇÃO ESTÁ O TIME DA Kiel
-		print(f'O time Kiel esta na °{grupoA.index("Kiel")+1} posição' ) 
+		print(f'O time Kiel esta na {grupoA.index("Kiel")+1}ª posição' ) 
 		print()
 		#USANDO O METODO 'INDEX' ELE MOSTRA EXATAMENTE A POSIÇÃO DO INDICE, ADICIONAR '+1' FICA LEGIVEL PARA O USUÁRIO
 		
 		
     elif grupo == 'B':
-        print('GRUPO B SELECIONADO: ')
+        print('Grupo B selecionado: ')
 		#A) OS 5 PRIMEIROS TIMES.	 
-		print(f'Os 5 primeiros times são {grupoB[0:5]}')
+		print(f'Os 5 primeiros times do grupo B são: {grupoB[0:5]}')
 		#B) OS ÚLTIMOS 4 COLOCADOS.
-		print(f'Os últimos 4 times na zona de rebaixamento são {grupoB[-4:]}')
+		print(f'Os últimos 4 times do grupo B são: {grupoB[-4:]}')
 		#C) TIMES EM ORDEM ALFABÉTICA.
 		print(f'Sequência de times em ordem alfabética: {sorted(grupoB)}')
 		#D) EM QUE POSIÇÃO ESTÁ O TIME DA Barcelona
-		print(f'O time Kiel esta na °{grupoB.index("Barcelona")+1} posição' )
+		print(f'O time Barcelona esta na {grupoB.index("Barcelona")+1}ª posição' )
 		
     else:
         print('Tente novamente')
+print('-'*20)
+
+#resolução do curso
 
 
+times = ('Corinthians','Palmeiras','Santos','Grêmio',
+		 'Cruzeiro','Flamengo','Vasco','Chapecoense',
+		 'Atlético','Botafogo','Atlético-PR','Bahia',
+		 'São Paulo','Fluminense','Sport Recife',
+		 'EC Vitória','Coritiba','Avaí','Ponte Preta',
+		 'Atlético-GO')
 
+print('-='*15)
+print(f'Lista de times do Brasileirão: {times}')
+print('-='*15)
+print(f'Os 5 primeiros são {times[0:5]}')
+print('-='*15)
+print(f'Os 4 últimos são {times[-4:]}')
+print('-='*15)
+print(f'Times em ordem alfabética: {sorted(times)}')
+print('-='*15)
+print(f'O Chapecoense está na {times.index("Chapecoense")}ª posição')
