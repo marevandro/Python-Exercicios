43239759097ff564c828c8e814d4396faa8b6884
Marevandro <62778708+marevandro@users.noreply.github.com>
2021-11-08 21:18:27 -0300

    Update EX-75 - Análise de dados em uma Tupla.py

--- 100 Exercicios/Mundo 3/EX-75 - Análise de dados em uma Tupla.py
+++ 100 Exercicios/Mundo 3/EX-75 - Análise de dados em uma Tupla.py
@@ -7,6 +7,18 @@ B) Em que posição foi digitado o primeiro valor 3.
 C) Quais foram os números pares.'''
 
 
-
-for i in range(1,5):
-	valor = int(input('Digite um valor: ')
+#solução do curso
+núm = (int(input('Digite um número: ')),
+	   int(input('Digite outro número: ')),
+	   int(input('Sigite mais um número: ')),
+	   int(input('Digite o último número: ')))
+print(f'Você digitou os valores {núm}')
+print(f'O valor 9 apareceu {núm.count(9)} vezes')
+if 3 in núm:
+	print(f'O valor 3 apareceu na {núm.index(3)+1}º posição')
+else:
+	print('O valor 3 não foi digitado em nenhuma posição')
+print(f'Os valores pares digitados foram ',end='')
+for n in núm:
+	if n % 2 == 0:
+		print(n, end='')
