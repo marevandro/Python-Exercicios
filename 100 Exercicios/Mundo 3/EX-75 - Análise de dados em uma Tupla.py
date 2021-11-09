ccae0daebc7a438d079a2c5edb1b122d29b13a7e
Marevandro <62778708+marevandro@users.noreply.github.com>
2021-11-08 22:18:50 -0300

    Update EX-75 - Análise de dados em uma Tupla.py
    Verificar porque minha resolução ainda da erro

--- 100 Exercicios/Mundo 3/EX-75 - Análise de dados em uma Tupla.py
+++ 100 Exercicios/Mundo 3/EX-75 - Análise de dados em uma Tupla.py
@@ -22,3 +22,24 @@ print(f'Os valores pares digitados foram ',end='')
 for n in núm:
 	if n % 2 == 0:
 		print(n, end='')
+		
+#minha resolução
+
+num1 = tuple(input('Digite o 1° número: '))
+num2 = tuple(input('Digite o 2° número: '))
+num3 = tuple(input('Digite o 3° número: '))
+num4 = tuple(input('Digite o 4° número: '))
+
+todos = num1 + num2 + num3 + num4
+
+print(f'O valor 9 apareceu {todos.count(9)}x')
+if 3 in todos:
+	print(f'O valor 3 esta na posição {todos.index(3)+1}° posição')
+else:
+	print('O valor 3 não foi digitado nenhuma vez')
+for par in todos:
+	if n % 2 == 0:
+		print(n ,) 
+		
+Validar pq minha resolução ainda da erro
+
