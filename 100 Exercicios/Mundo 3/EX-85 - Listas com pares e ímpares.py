8edb02abdefcd304b71f6a46adb14105a62eef4d
Marevandro <62778708+marevandro@users.noreply.github.com>
2022-02-24 17:54:24 -0300

    add

--- 100 Exercicios/Mundo 3/EX-85 - Listas com pares e ímpares.py
+++ 100 Exercicios/Mundo 3/EX-85 - Listas com pares e ímpares.py
@@ -27,4 +27,19 @@ print('-'*5)
 print(f'Os valores impares em ordem crescente são: {impar}')
 print('-'*5)
 
+#------------------------------- resolução do curso ----------------------------------------
 
+núm = [[],[]]
+valor = 0
+for c in range(1,8):
+    valor = int(input('Digite o {c}°. valor: '))
+    if valor % 2 == 0:
+        núm[0].append(valor)
+    else:
+        núm[1].append(valor)
+print('-='*30)
+núm[0].sort()
+núm[1].sort()
+print(f'Os valores pares digitados foram: {núm[0]}')
+print(f'Os valores ímpares digitados foram: {núm[1]'}
+        
